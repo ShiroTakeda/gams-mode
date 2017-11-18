900e1788695d91dde2a3d7fee97681a3380285de
ShiroTakeda <shiro.takeda@gmail.com>
2017-11-18 11:15:08 +0900

    Version 6.3

--- gams-mode.el
+++ gams-mode.el
@@ -5,8 +5,8 @@
 ;; Maintainer: Shiro Takeda
 ;; Copyright (C) 2001-2017 Shiro Takeda
 ;; First Created: Sun Aug 19, 2001 12:48 PM
-;; Time-stamp: <2017-08-06 12:51:58 st>
-;; Version: 6.2
+;; Time-stamp: <2017-11-18 11:14:33 st>
+;; Version: 6.3
 ;; Keywords: GAMS
 ;; URL: http://shirotakeda.org/en/gams/gams-mode/
 ;; This file is not part of any Emacs.
@@ -74,7 +74,7 @@
 ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defconst gams-mode-version "6.2"
+(defconst gams-mode-version "6.3"
   "Version of GAMS mode.")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
