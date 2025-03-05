7d1700bb1640e5c4dd5902ce3f0b8631f01cc37c
Christophe Gouel <christophe.gouel@inrae.fr>
2025-03-05 18:44:44 +0100

    feat(gams-mode): add detection for performance-related messages in GAMS process
    Added a new regular expression to detect performance-related messages in the GAMS process buffer using compile mode. This helps in identifying notes related to memory and execution time.

--- gams-mode.el
+++ gams-mode.el
@@ -3869,11 +3869,17 @@ PROC is the process name and STATE is the process state."
 
 ;; Error detection in process buffer using compile mode
 (defvar gams-process-error-regexp
-  '("^--- \\(.*\\)(\\([0-9]+\\)) .*Error\\(s?\\)$" 1 2)
+  '("^--- \\(.+?\\)(\\([0-9]+\\)) .*Error\\(s?\\)$" 1 2)
   "Regexp to detect errors in GAMS process using compile mode.")
+(defvar gams-process-note-regexp
+  '("^--- \\(.+?\\)(\\([0-9]+\\)) +[0-9]+ Mb\\(?: +[0-9]+ secs\\)?$" 1 2 nil 0)
+  "Regexp to detect performance-related messages in GAMS process using compile mode as notes.")
 (add-to-list 'compilation-error-regexp-alist-alist
              (cons 'gams gams-process-error-regexp))
 (add-to-list 'compilation-error-regexp-alist 'gams)
+(add-to-list 'compilation-error-regexp-alist-alist
+             (cons 'gams-note gams-process-note-regexp))
+(add-to-list 'compilation-error-regexp-alist 'gams-note)
 
 (defun gams-process-error-exist-p ()
   "Judge whether GAMS process ends with errors."
